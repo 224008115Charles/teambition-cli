a6736f1d9b5fa063ad2b196c956bf6577a6aa9be
qiaoqiao <qiao@teambition.com>
2018-11-09 18:20:11 +0800

    fix: 更正  版本显示错误的问题

--- bin/teambition-cli.js
+++ bin/teambition-cli.js
@@ -3,7 +3,9 @@
 // node ./bin/command.js init my-project
 
 const program = require('commander')
-program.version('0.0.1')
+const pkg = require('../package.json')
+
+program.version(pkg.version)
 	.usage('<command> [project-name]')
 	.command('test', 'this will run teambition-cli-test.js')
 	.command('init', 'this will run teambition-cli-init.js')
